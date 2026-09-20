e91c20df97e7b3d092dc721739a1fdc948b5646d
rlatngud1995 <rlatngud8557@naver.com>
2026-09-21 08:48:37 +0900

    Change SITE_URL to new domain
    Updated the SITE_URL to point to the new domain.

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,18 +1,17 @@
+
 import type { MetadataRoute } from "next";
 
-const SITE_URL = "https://jipsurimoa.vercel.app";
+const SITE_URL = "https://www.jipsurimoa.com";
 
 export default function sitemap(): MetadataRoute.Sitemap {
   return [
     {
       url: SITE_URL,
-      lastModified: new Date(),
       changeFrequency: "weekly",
       priority: 1,
     },
     {
       url: `${SITE_URL}/companies`,
-      lastModified: new Date(),
       changeFrequency: "daily",
       priority: 0.9,
     },
