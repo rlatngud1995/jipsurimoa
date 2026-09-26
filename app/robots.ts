67ecdef76968837895caa67cfb4a791dbf9078e1
rlatngud1995 <rlatngud8557@naver.com>
2026-09-26 12:47:10 +0900

    Refactor robots rules to use array format

--- app/robots.ts
+++ app/robots.ts
@@ -1,15 +1,23 @@
-
 import type { MetadataRoute } from "next";
 
 const SITE_URL = "https://www.jipsurimoa.com";
 
 export default function robots(): MetadataRoute.Robots {
   return {
-    rules: {
-      userAgent: "*",
-      allow: "/",
-      disallow: ["/admin", "/api/"],
-    },
+    rules: [
+      {
+        userAgent: "*",
+
+        allow: "/",
+
+        disallow: [
+          "/admin",
+          "/admin/",
+          "/api",
+          "/api/",
+        ],
+      },
+    ],
 
     sitemap: [
       `${SITE_URL}/sitemap.xml`,
